d8317883f08e6150b3bae5120c0521c2693f9d23
kellyg418 <121138635+kellyg418@users.noreply.github.com>
2023-04-29 23:39:22 -0700

    Created necessary files to create the basics of the search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello World!
+      <div className="container">
+        <header className="App-header"></header>
+        Hello World!
+      </div>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center">
+        This project was coded by Kelly Granados and is open-sourced on GitHub
+      </footer>
     </div>
   );
 }
